52649b638ae94ef17ae3be7388174755bba33a4d
Derek-Stanley <71318168+Derek-Stanley@users.noreply.github.com>
2022-01-11 12:34:11 +0530

    Update entailment.py

--- entailment.py
+++ entailment.py
@@ -1,85 +1,56 @@
-combinations=[(True,True, True),(True,True,False),(True,False,True),(True,False, False),(False,True, True),(False,True, False),(False, False,True),(False,False, False)]
-variable={'p':0,'q':1, 'r':2}
-kb=''
-q=''
-priority={'~':3,'v':1,'^':2}
-def input_rules():
-  global kb, q
-  kb = (input("Enter Rule :"))
-  q = input("Enter Query : ")
+import re
+def UNIFY(x,y,sub):
+    print(x,y,sub)
+    if sub == -1:print("minus 1"); return -1
+    elif x == y:print("same"); return sub
+    elif VARIABLE(x):print("var x"); return UNIFY_VAR(x,y,sub)
+    elif VARIABLE(y):print("var y"); return UNIFY_VAR(y,x,sub) 
+    elif COMPOUND(x) and COMPOUND(y):
+        print("coumpound");
+        return UNIFY(ARGS(x),ARGS(y),UNIFY(OP(x),OP(y),sub))
+    elif LIST(x) and LIST(y) and len(x) == len(y):
+        print("list");
+        return UNIFY(x[1:],y[1:],UNIFY(x[0],y[0],sub))
+    else:print("none"); return -1
 
-def entailment():
-  global kb, q
-  print('*' * 10 + "Truth Table Reference" + "*" * 10)
-  print("kb", "alpha")
-  print("*" * 10)
-  for comb in combinations:
-    s = evaluatePostfix(toPostfix(kb), comb)
-    f = evaluatePostfix(toPostfix(q), comb)
-    print(s, f)
-    print("-" * 10)
-    if s and not f:
-      return False
-  return True
-def isOperand(c):
-  return c.isalpha() and c != 'v'
+def VARIABLE(x):
+    if type(x) == list: return False
+    elif re.search("^[a-z]$",x): return True
+    else: return False
 
-def isLeftParenthesis(c):
-  return c == "("
+def COMPOUND(x):
+    if type(x) == list: return False
+    return re.search("^.+\(.*\)$", x)
 
-def isRightParenthesis(c):
-  return c == ")"
-
-def isEmpty(stack):
-  return len(stack) == 0
-
-def peek(stack):
-  return stack[-1]
-
-def hasLessOrEqualPriority(c1, c2):
-  try: return priority[c1] <= priority[c2]
-  except KeyError: return False
-
-def toPostfix(infix):
-  stack = []
-  postfix = ''
-  for c in infix:
-    if isOperand(c):
-      postfix += c
+def ARGS(x):
+    arg = re.findall("\((.*)\)", x)[0].split(",")
+    if len(arg) == 1:
+        return arg[0]
     else:
-      if isLeftParenthesis(c):
-        stack.append(c)
-      elif isRightParenthesis(c):
-        operator = stack.pop()
-        while not isLeftParenthesis(operator):
-          postfix += operator
-          operator = stack.pop()
-      else:
-        while (not isEmpty(stack)) and hasLessOrEqualPriority(c, peek(stack)):
-          postfix += stack.pop()
-        stack.append(c)
-  while (not isEmpty(stack)):
-    postfix += stack.pop()
-  return postfix
+        return arg
 
-def _eval(i, val1, val2):
-    if i == '^': return val2 and val1
-    return val2 or val1
-    
-def evaluatePostfix(exp, comb):
-  stack = []
-  for i in exp:
-    if isOperand(i):
-      stack.append(comb[variable[i]])
-    elif i == '~':
-      val1 = stack.pop()
-      stack.append(not val1)
+def OP(x):
+    return re.findall("^([a-zA-Z]+)\(.*\)$", x)[0]
+
+def LIST(x):
+    return True if type(x) == list else False
+def UNIFY_VAR(var, x, sub):
+    if var in sub: return UNIFY(sub[var],x,sub)
+    elif x in sub: return UNIFY(var,sub[var],sub)
+    elif OCCUR_CHECK(var,x): return -1
     else:
-      val1 = stack.pop()
-      val2 = stack.pop()
-      stack.append(_eval(i, val2, val1))
-  return stack.pop()
-input_rules()
-ans = entailment()
-if ans: print("The Knowledge Base Entails Query")
-else: print("The Knowledge Base Doesn't Entail Query")
+        sub[var]=x
+        return sub
+
+def OCCUR_CHECK(x,y):
+    if x in y: return True
+    else: return False
+
+s1 = "Knows(x,Father(y),d)"
+s2 = "Knows(John,Father(Hello),Mother(Mary))"
+substitution = UNIFY(s1,s2,{})
+if (substitution == -1):
+    print("Substitution not possible!")
+else:
+    print("Successfull:", substitution)
+
